79a16d14bab14d1266000bbdf4fdf9ef678d0782
gyana prakash gouda <gyanaprakashgouda471@gmail.com>
2014-11-03 18:08:00 +0530

    live-weather-report-for-seven-day
    api tutorial

--- weather/AppDelegate.m
+++ weather/AppDelegate.m
@@ -7,15 +7,22 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
+
 
 @end
 
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    ViewController *vc=[[ViewController alloc]initWithNibName:@"ViewController" bundle:nil];
+    UINavigationController *navi = [[UINavigationController alloc]initWithRootViewController:vc];
+    self.window.rootViewController = navi;
+    [self.window makeKeyAndVisible];
     // Override point for customization after application launch.
     return YES;
 }
